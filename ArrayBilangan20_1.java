2409b34d0d66fb58700fcab0d5255b2096ffac1a
blietz123 <144086689+blietz123@users.noreply.github.com>
2023-10-24 15:18:48 +0700

    Jobsheet 9

---
+++ ArrayBilangan20_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ArrayBilangan20_1 {
+    public static void main(String[] args) {
+        int [] bil = {5, 13, -7, 17};
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
+
+
+    }
+}
